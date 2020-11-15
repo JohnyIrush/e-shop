0f87164190f73b42ead98dc00d02e9e26d9d7677
JohnyIrush <johnydanxin@gmail.com>
2020-11-16 00:23:12 +0300

    Flash messages

--- resources/views/shop/pages/home.blade.php
+++ resources/views/shop/pages/home.blade.php
@@ -24,8 +24,11 @@
      <div class="col-xs-12 col-sm-9 mt-2 col-md-12 col-lg-9">
       <div class="row content-justify-center mt-1 ">
         @if (session()->has('message'))
-        <div class="alert alert-success alert-dismissible fade show col-12 " role="alert">
+        <div class="alert alert-success alert-dismissible fade show col-12 text-center" role="alert">
           {{session()->get('message')}}
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
         </div> 
         @endif
         <!--Search Bar-->
